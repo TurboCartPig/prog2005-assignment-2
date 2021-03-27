2a4e5d73a6be69f1eecaae99b72c9c0ae5e13833
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-03-27 20:57:38 +0100

    Implement reading all the webhooks

--- notifications/read.go
+++ notifications/read.go
@@ -10,8 +10,7 @@ import (
 	"net/http"
 )
 
-// Just fetch the document by document id and return it.
-
+// NewReadHandler creates a HttpHandler that reads one webhook from the database and returns it.
 func NewReadHandler(fs *firestore.Client) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		id := chi.URLParam(r, "id")
@@ -39,10 +38,43 @@ func NewReadHandler(fs *firestore.Client) http.HandlerFunc {
 	}
 }
 
-// Just fetch the collection and return all documents in it.
-
+// NewReadAllHandler creates a HttpHandler that reads all the webhooks from the database and returns them.
 func NewReadAllHandler(fs *firestore.Client) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
-		http.Error(rw, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
+		// Get refs to all the documents in the collection
+		docrefs, err := fs.Collection(WebhookCollection).DocumentRefs(r.Context()).GetAll()
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(rw, "Something went wrong trying to get the webhooks", http.StatusInternalServerError)
+			return
+		}
+
+		// For all the docrefs, get their document snapshot,
+		// and read the data into a struct, then append that struct to `body`
+		// NOTE: There might be docrefs for which there is no document, therefore we can not preallocate the body slice.
+		body := make([]requestBody, 0)
+		for _, docref := range docrefs {
+			docsnap, err := docref.Get(r.Context())
+			if status.Code(err) == codes.NotFound {
+				http.Error(rw, "Invalid webhook id; No webhook registered by that id", http.StatusBadRequest)
+				return
+			} else if err != nil {
+				log.Println(err.Error())
+				http.Error(rw, "Something went wrong trying to get the webhook", http.StatusInternalServerError)
+				return
+			}
+
+			var data requestBody
+			err = docsnap.DataTo(&data)
+			if err != nil {
+				log.Println(err.Error())
+				http.Error(rw, "Something went wrong trying to read the webhook", http.StatusInternalServerError)
+				return
+			}
+
+			body = append(body, data)
+		}
+
+		_ = json.NewEncoder(rw).Encode(&body)
 	}
 }
